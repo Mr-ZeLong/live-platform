a3edbd9535910ffc060adcc2187e54f680c5da81
Mr-ZeLong <473840782@qq.com>
2025-06-14 22:57:00 +0800

    1. 新增com.logilong.live.framework.redis.starter.key.AccountProviderCacheKeyBuilder类，生成Account相关业务的redisId 2. 新增live-account-provider和live-account-interface模块，提供用户Token生成以及获取RPC接口

---
+++ live-account-provider/src/main/java/com/logilong/live/account/provider/rpc/AccountTokenRPCImpl.java
@@ -0,0 +1,23 @@
+package com.logilong.live.account.provider.rpc;
+
+import jakarta.annotation.Resource;
+import org.apache.dubbo.config.annotation.DubboService;
+import com.logilong.live.account.interfaces.IAccountTokenRPC;
+import com.logilong.live.account.provider.service.IAccountTokenService;
+
+@DubboService
+public class AccountTokenRPCImpl implements IAccountTokenRPC {
+
+    @Resource
+    private IAccountTokenService accountTokenService;
+
+    @Override
+    public String createAndSaveLoginToken(Long userId) {
+        return accountTokenService.createAndSaveLoginToken(userId);
+    }
+
+    @Override
+    public Long getUserIdByToken(String tokenKey) {
+        return accountTokenService.getUserIdByToken(tokenKey);
+    }
+}
