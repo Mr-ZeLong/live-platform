5f1c372b66e6c0f3b9aab49e036ed77e8742d2c8
Mr-ZeLong <473840782@qq.com>
2025-06-30 16:11:19 +0800

    bugfix

--- live-living-provider/src/main/java/com/logilong/live/living/provider/consumer/StartLivingRoomConsumer.java
+++ live-living-provider/src/main/java/com/logilong/live/living/provider/consumer/StartLivingRoomConsumer.java
@@ -1,48 +1,47 @@
-package com.logilong.live.gift.provider.consumer;
+package com.logilong.live.living.provider.consumer;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.logilong.live.common.interfaces.topic.LivingProviderTopicNames;
+import com.logilong.live.sku.interfaces.ISkuStockInfoRPC;
 import jakarta.annotation.Resource;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
-import com.logilong.live.framework.redis.starter.key.SkuProviderCacheKeyBuilder;
-import com.logilong.live.common.interfaces.topic.SkuProviderTopicNames;
+import org.apache.rocketmq.common.message.MessageExt;
 import com.logilong.live.framework.mq.starter.properties.RocketMQConsumerProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 
-/**
- * 在容器初始化的时候，消费关于库存回滚的主题消息
- */
+
 @Configuration
-public class StockRollbackConsumer implements InitializingBean {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(StockRollbackConsumer.class);
-
+public class StartLivingRoomConsumer implements InitializingBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartLivingRoomConsumer.class);
     @Resource
     private RocketMQConsumerProperties rocketMQConsumerProperties;
-    @Resource
-    private ISkuStockInfoService skuStockInfoService;
+    @DubboReference
+    ISkuStockInfoRPC skuStockInfoRPC;
 
     @Override
     public void afterPropertiesSet() throws Exception {
         DefaultMQPushConsumer mqPushConsumer = new DefaultMQPushConsumer();
-        //老版本中会开启，新版本的mq不需要使用到
         mqPushConsumer.setVipChannelEnabled(false);
         mqPushConsumer.setNamesrvAddr(rocketMQConsumerProperties.getNameSrv());
-        mqPushConsumer.setConsumerGroup(rocketMQConsumerProperties.getGroupName() + "_" + StockRollbackConsumer.class.getSimpleName());
-        //一次从broker中拉取10条消息到本地内存当中进行消费
+        mqPushConsumer.setConsumerGroup(rocketMQConsumerProperties.getGroupName() + "_" + StartLivingRoomConsumer.class.getSimpleName());
         mqPushConsumer.setConsumeMessageBatchMaxSize(10);
         mqPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
         //监听礼物缓存数据更新的行为
-        mqPushConsumer.subscribe(SkuProviderTopicNames.ROLL_BACK_STOCK, "");
+        mqPushConsumer.subscribe(LivingProviderTopicNames.START_LIVING_ROOM, "");
         mqPushConsumer.setMessageListener((MessageListenerConcurrently) (msgs, context) -> {
-            RockBackInfoDTO rockBackInfoDTO = JSON.parseObject(new String(msgs.get(0).getBody()), RockBackInfoDTO.class);
-            skuStockInfoService.stockRollbackHandler(rockBackInfoDTO);
+            for (MessageExt msg : msgs) {
+                JSONObject jsonObject = JSON.parseObject(new String(msg.getBody()));
+                Long anchorId = jsonObject.getLong("anchorId");
+                skuStockInfoRPC.prepareStockInfo(anchorId);
+            }
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
         mqPushConsumer.start();
