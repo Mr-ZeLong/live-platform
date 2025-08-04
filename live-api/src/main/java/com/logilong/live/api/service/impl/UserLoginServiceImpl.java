c5265dd3ef1f2fcd49c497663691479793522216
Mr-ZeLong <473840782@qq.com>
2025-08-04 21:56:59 +0800

    chore: 添加 cookie 域名日志输出
    - 在设置 cookie 域名后，增加日志输出语句
    -通过日志记录 cookie 的域名设置情况

--- live-api/src/main/java/com/logilong/live/api/service/impl/UserLoginServiceImpl.java
+++ live-api/src/main/java/com/logilong/live/api/service/impl/UserLoginServiceImpl.java
@@ -67,6 +67,7 @@ public class UserLoginServiceImpl implements IUserLoginService {
         //http://api.live.com/live/api/userLogin/sendLoginCode
         // 用于指定Cookie的有效域名范围，控制哪些域名可以访问该Cookie
         cookie.setDomain(webDomain);
+        log.info("cookie的domain:{}", cookie.getDomain());
         cookie.setPath("/");
 
         //cookie有效期，一般他的默认单位是秒，设置为30天
